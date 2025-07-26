cdb2f03b4e2c610e9a6301c35d273d3a76d84a15
rohan baradeli <rohanbaradeli@gmail.com>
2025-07-26 11:05:58 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -1,1 +1,10 @@
+// app.js
 
+document.addEventListener("DOMContentLoaded", () => {
+  const button = document.getElementById("clickButton");
+  const message = document.getElementById("message");
+
+  button.addEventListener("click", () => {
+    message.textContent = "Button was clicked!";
+  });
+});
